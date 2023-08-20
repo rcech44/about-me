d1294b54df3a31928047454031d4691e0179e3a1
Radomir Cech <rcech44@gmail.com>
2023-08-20 17:36:14 +0200

    Updated projects

--- js/scripts.js
+++ js/scripts.js
@@ -44,3 +44,39 @@ function pageTransition(el)
         return false;
     }
 }
+
+function setModal(type)
+{
+    var pic_1 = document.getElementById("modal_pic_1");
+    var pic_2 = document.getElementById("modal_pic_2");
+    var pic_3 = document.getElementById("modal_pic_3");
+    var pic_4 = document.getElementById("modal_pic_4");
+    var short_pic_1 = document.getElementById("short_modal_pic_1");
+    var short_pic_2 = document.getElementById("short_modal_pic_2");
+    if (type == "covid")
+    {
+        pic_1.src = "assets/img/covid_showcase/1.jpg";
+        pic_2.src = "assets/img/covid_showcase/2.jpg";
+        pic_3.src = "assets/img/covid_showcase/3.jpg";
+        pic_4.src = "assets/img/covid_showcase/4.jpg";
+    }
+    else if (type == "rtsp")
+    {
+        pic_1.src = "assets/img/rtsp_showcase/1.jpg";
+        pic_2.src = "assets/img/rtsp_showcase/2.jpg";
+        pic_3.src = "assets/img/rtsp_showcase/3.jpg";
+        pic_4.src = "assets/img/rtsp_showcase/4.jpg";
+    }
+    else if (type == "game_engine")
+    {
+        short_pic_1.src = "assets/img/engine_showcase/1.jpg";
+        short_pic_2.src = "assets/img/engine_showcase/2.jpg";
+    }
+    else if (type == "games")
+    {
+        pic_1.src = "assets/img/game_showcase/1.jpg";
+        pic_2.src = "assets/img/game_showcase/2.jpg";
+        pic_3.src = "assets/img/game_showcase/3.jpg";
+        pic_4.src = "assets/img/game_showcase/4.jpg";
+    }
+}
